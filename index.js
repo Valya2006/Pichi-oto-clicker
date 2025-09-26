855c143c8a4dbc530ca43e0e0c00f5b5a27094a6
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2025-09-26 14:22:21 +0300

    Basic

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 class PetCounter {
 
     constructor() {
-    this.button = document.querySelector('.pet-button')
+    this.buttonImg = document.querySelector('.pet-button')
 		this.counter = document.querySelector('.counter')
+		this.buttonReset = document.querySelector('.btn-reset')
+		this.buttonMylty = document.querySelector('.btn-ultiplier')
+		this.buttonToggle = document.querySelector('.pet-toggle')
 		this.count = 0
 
 		this.init();
   }
 
 	init() {
-		this.button.addEventListener('click', () => this.addPoint())
+		this.buttonImg.addEventListener('click', () => this.addPoint())
+		this.buttonReset.addEventListener('click', () => this.reset())
+		this.buttonMylty.addEventListener('click', () => this.mylti())
+		this.buttonToggle.addEventListener('click', () => this.location())
 	}
 
 	addPoint() {
@@ -26,7 +32,25 @@ class PetCounter {
 				this.counter.style.transform = 'scale(1)';
 			}, 100);
 	}
+
+	reset() {
+		this.count = 0;
+		this.counter.textContent = 0;
+	}
+
+	mylti() {
+		this.count *= 2
+		this.counter.textContent = this.count
+	}
+
+	location() {
+		const locatoin = window.location.href
+		if (locatoin.includes('Oto-clicker.html')) {
+			window.location.href = 'Pichi-clickers.html';
+		} else {
+			window.location.href = 'Oto-clicker.html';
+		}
+	}
 }
 
 new PetCounter()
-
